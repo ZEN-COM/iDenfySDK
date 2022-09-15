5c7c9fedef87ebd36e385b196b5c8504a21f94b6
Piotr Ożóg <piotr.ozog@zen.com>
2022-09-15 16:16:55 +0200

    update targets

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "iDenfySDK",
-            targets: ["iDenfySDK"]),
+            targets: ["FaceTecSDK", "idenfycore", "IdenfyLiveness", "idenfyNFCReading", "iDenfySDK", "idenfyviews"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
